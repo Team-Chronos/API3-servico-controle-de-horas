c8ad570960bacddaf871694c7c3afa4ba3d35d95
Rafael Sette de Araujo <138353834+Sette0o0@users.noreply.github.com>
2026-03-20 15:41:33 -0300

    feat (#39): serviço buscador de registros

---
+++ controle-horas/src/main/java/com/api/controle_horas/servicos/BuscadorControleHoras.java
@@ -0,0 +1,49 @@
+package com.api.controle_horas.servicos;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.controle_horas.mappers.ControleHorasMapper;
+import com.api.controle_horas.modelos.dto.ControleHorasDto;
+import com.api.controle_horas.modelos.dto.RegistrosHorasDto;
+import com.api.controle_horas.modelos.entidades.ControleHoras;
+import com.api.controle_horas.repositorios.ControleHorasRepositorio;
+
+@Service
+public class BuscadorControleHoras {
+
+  @Autowired
+  ControleHorasRepositorio repositorio;
+
+  @Autowired
+  CalculadorTempo calculadorTempo;
+
+  public ControleHorasDto buscarPorId(Long id) {
+    ControleHoras controleHoras = repositorio.findById(id).orElseThrow(() -> new RuntimeException("Registro não encontrado"));
+
+    ControleHorasDto controleHorasDto = ControleHorasMapper.toDto(controleHoras);
+
+    calculadorTempo.aplicarTempo(controleHorasDto);
+
+    return controleHorasDto;
+  }
+
+  public RegistrosHorasDto buscarPorTarefa(Long tarefa_id) {
+    List<ControleHoras> controleHorases = repositorio.findByTarefaId(tarefa_id);
+    RegistrosHorasDto registrosHorasDto = ControleHorasMapper.toDto(controleHorases);
+    Duration tempoTotal = Duration.ZERO;
+    for (ControleHorasDto controleHorasDto : registrosHorasDto.getRegistros()) {
+
+      Duration tempo = calculadorTempo.aplicarTempo(controleHorasDto);
+
+      if (tempo != null) {
+        tempoTotal = tempoTotal.plus(tempo);
+      }
+    }
+    registrosHorasDto.setTempoMinutos(tempoTotal.toMinutes());
+    return registrosHorasDto;
+  }
+}
