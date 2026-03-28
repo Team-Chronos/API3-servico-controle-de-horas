2db2c09f00ad4d6357325b5543e8ad27b96417b1
Rafael Sette de Araujo <138353834+Sette0o0@users.noreply.github.com>
2026-03-28 09:11:19 -0300

    feat (#40): validação de existência de tarefa

--- controle-horas/src/main/java/com/api/controle_horas/servicos/CriadorControleHoras.java
+++ controle-horas/src/main/java/com/api/controle_horas/servicos/CriadorControleHoras.java
@@ -2,6 +2,8 @@ package com.api.controle_horas.servicos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClientException;
 
 import com.api.controle_horas.mappers.ControleHorasMapper;
 import com.api.controle_horas.modelos.dto.ControleHorasRequestDto;
@@ -10,12 +12,24 @@ import com.api.controle_horas.repositorios.ControleHorasRepositorio;
 
 @Service
 public class CriadorControleHoras {
-  
+
   @Autowired
   ControleHorasRepositorio repositorio;
 
-  public void criar(ControleHorasRequestDto requestDto){
-    ControleHoras controleHoras = ControleHorasMapper.toEntity(requestDto);
-    repositorio.save(controleHoras);
+  public void criar(ControleHorasRequestDto requestDto) {
+    RestTemplate restTemplate = new RestTemplate();
+
+    try {
+      restTemplate.getForObject(
+        "http://localhost:8081/tarefas/" + requestDto.getTarefa_id(),
+        String.class
+      );
+
+      ControleHoras controleHoras = ControleHorasMapper.toEntity(requestDto);
+      repositorio.save(controleHoras);
+
+    } catch (RestClientException e) {
+      throw new RuntimeException("Tarefa não encontrada");
+    }
   }
 }
